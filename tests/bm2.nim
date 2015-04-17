598253b4d3c1d1c3e569421db50855703477739c
Wink Saville <wink@saville.com>
2015-04-17 13:00:36 -0700

    Have bm2.nim be a simple looper test
    bm2.nim was t3.nim and the old bm2.nim will become bm3.nim.

--- tests/bm2.nim
+++ tests/bm2.nim
@@ -1,21 +1,11 @@
-import msg, linknode, mpscfifo, msgarena, msglooper, benchmark, os, locks
-
-# include bmSuite so we can use it inside t(name: string)
-include "bmsuite"
-
-const
-  runTime = 2.00 #30.0
-  warmupTime = 0.0 #0.25
-  threadCount = 1
-  testStatsCount = 1
+import msg, msgarena, mpscfifo, msglooper, benchmark, os, locks
 
 var
   ma: MsgArenaPtr
   ml1: MsgLooperPtr
   ml1RsvQ: QueuePtr
 
-var
-  ml1PmCount = 0
+var ml1PmCount = 0
 proc ml1Pm(msg: MsgPtr) =
   echo "ml1Pm: **** msg=", msg
   ml1PmCount += 1
@@ -23,59 +13,36 @@ proc ml1Pm(msg: MsgPtr) =
 
 ma = newMsgArena()
 ml1 = newMsgLooper("ml1")
-ml1RsvQ = newMpscFifo("ml1RsvQ", ma)
+ml1RsvQ = newMpscFifo("ml1RsvQ", ma, false, ml1.cond, ml1.lock, blockIfEmpty)
 ml1.addProcessMsg(ml1Pm, ml1RsvQ)
 
-type
-  TObj = object
-    name: string
-    index: int32
+suite "msglooper", 0.0:
+  var
+    msg: MsgPtr
+    tsa: array[0..0, TestStats]
+    rspQ1: QueuePtr
 
-proc newTObj(name: string, index: int): TObj =
-  result.name = name
-  result.index = cast[int32](index and 0xFFFFFFFF)
+  setup:
+    echo suiteObj.suiteName & " setup:+"
+    rspQ1 = newMpscFifo("rspQ1", ma, blockIfEmpty)
+    msg = ma.getMsg(1, 0)
+    msg.rspQ = rspQ1
+    echo suiteObj.suiteName & " setup:-"
 
-proc t(tobj: TObj) {.thread.} =
-  echo "t+ tobj=", tobj
+  teardown:
+    echo suiteObj.suiteName & " teardown+"
+    ma.retMsg(msg)
+    rspQ1.delMpscFifo()
+    echo suiteObj.suiteName & " teardown-"
 
-  bmSuite tobj.name, warmupTime:
-    echo suiteObj.suiteName & ".suiteObj=" & $suiteObj
-    var
-      msg: MsgPtr
-      rspq: QueuePtr
-      tsa: array[0..testStatsCount-1, TestStats]
+  test "test1", 1, tsa:
+    echo suiteObj.fullName & " call add msg=" & $msg
+    ml1RsvQ.add(msg)
+    echo suiteObj.fullName & " call rmv"
+    msg = rspQ1.rmv()
+    echo suiteObj.fullName & " ret  rmv msg=" & $msg
 
-    setup:
-      rspq = newMpscFifo("rspq-" & suiteObj.suiteName, ma)
-      msg = ma.getMsg(tobj.index, 0)
-      msg.rspq = rspq
-
-    teardown:
-      ma.retMsg(msg)
-      rspq.delMpscFifo()
-
-    # One loop for the moment
-    test "ping-pong", 1, tsa: #runTime, tsa:
-      ml1RsvQ.add(msg)
-      echo "waiting for response!!"
-      while true:
-        msg = rspq.rmv()
-        if msg != nil:
-          break
-
-  echo "t:- tobj=", tobj
-
-var
-  idx = 0
-  threads: array[0..threadCount-1, TThread[TObj]]
-
-for idx in 0..threads.len-1:
-  var tobj = newTObj("X" & $idx, idx)
-  createThread[TObj](threads[idx], t, tobj)
-
-sleep(round(runTime * 1000.0 * 1.20))
-
-echo "cleanup ml1PmCount=", ml1PmCount
+echo "cleanup"
 
 ml1RsvQ.delMpscFifo()
 ml1.delMsgLooper()
