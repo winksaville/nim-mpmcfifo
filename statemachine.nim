bc46f085d931e306d0c5c322509ea5bfd5528b85
Wink Saville <wink@saville.com>
2015-05-04 19:37:33 -0700

    Partial hierarchical support.
    Added a states Table which with a key of a state and a parentStateInfo.
    Currently nothing is done with the hierarchy but you can add states and
    set their parent.
    
    Added some tests for one, two states plus a default with two child states
    also this last test make no attempt to pass "unhandled" messages which
    should be handled by the parent. Something for the future.
    
    Add StateMachine.java the hierarchical state machine I created for Android.
    This is there as an example.

--- statemachine.nim
+++ statemachine.nim
@@ -2,29 +2,54 @@
 ## a process to exeucte. This will evolve into a hierarchical
 ## state machine with enter and exit methods and problably
 ## using templates or macros to make it easy to use.
-import msg, msgarena, msglooper, mpscfifo, fifoutils
+import msg, msgarena, msglooper, mpscfifo, fifoutils, tables, hashes
 
 type
+  StateInfo[TypeState] = object of RootObj
+    state: TypeState
+    parentStateInfo: ref StateInfo[TypeState]
+
   StateMachine*[TypeState] = object of Component
     protocols*: seq[int]
     curState*: TypeState
     ma*: MsgArenaPtr
     ml*: MsgLooperPtr
+    states: TableRef[TypeState, ref StateInfo[TypeState]]
+
 
 proc dispatcher[TypeStateMachine](cp: ComponentPtr, msg: MsgPtr) =
   ## dispatcher cast cp to a TypeStateMachine and call current state
   var sm = cast[ptr TypeStateMachine](cp)
   sm.curState(sm, msg)
 
+proc newStateInfo[TypeState](sm: ptr StateMachine[TypeState],
+    state: TypeState, parent: TypeState): ref StateInfo[TypeState] =
+  var
+    parentStateInfo: ref StateInfo[TypeState]
+
+  new(result)
+  if parent != nil and hasKey[TypeState, ref StateInfo[TypeState]](sm.states, parent):
+    echo "parent exists"
+    parentStateInfo = mget[TypeState, ref StateInfo[TypeState]](sm.states, parent)
+  else:
+    echo "parent not in table"
+    parentStateInfo = nil
+  result.state = state
+  result.parentStateInfo = parentStateInfo
+
+proc `$`*(si: ref StateInfo): string =
+  result = "{state=" # & si.state
+
 proc initStateMachine*[TypeStateMachine, TypeState](sm: ptr StateMachine[TypeState],
-    name: string, initialState: TypeState) =
+    name: string) =
   ## Initialize StateMachine
+  sm.states = newTable[TypeState, ref StateInfo[TypeState]]()
   sm.name = name
   sm.pm = dispatcher[TypeStateMachine]
   sm.ma = newMsgArena()
   sm.ml = newMsgLooper("ml_" & name)
   sm.rcvq = newMpscFifo("fifo_" & name, sm.ma, sm.ml)
-  sm.curState = initialState
+  sm.curState = nil
   sm.ml.addProcessMsg(sm)
 
 proc deinitStateMachine*[TypeState](sm: ptr StateMachine[TypeState]) =
@@ -32,6 +57,21 @@ proc deinitStateMachine*[TypeState](sm: ptr StateMachine[TypeState]) =
   sm.ml.delMsgLooper()
   sm.rcvq.delMpscFifo()
   sm.ma.delMsgArena()
+
+proc startStateMachine*[TypeState](sm: ptr StateMachine[TypeState], initialState: TypeState) =
+  ## Start the state machine at initialState
+  ## TODO: More to do when hierarchy is implemented
+  sm.curState = initialState
+
+proc addState*[TypeState](sm: ptr StateMachine[TypeState], state: TypeState, parent: TypeState = nil) =
+  ## Add a new state to the hierarchy. The parent argument may be nil
+  ## if the state has no parent.
+  if hasKey[TypeState, ref StateInfo[TypeState]](sm.states, state):
+    echo "addState: state already added"
+  else:
+    echo "addState: adding state"
+    var stateInfo = newStateInfo[TypeState](sm, state, parent)
+    add[TypeState, ref StateInfo[TypeState]](sm.states, state, stateInfo)
 
 proc transitionTo*[TypeState](sm: ptr StateMachine[TypeState], state: TypeState) =
   ## Transition to a new state
@@ -46,12 +86,21 @@ when isMainModule:
       SmT1 = object of StateMachine[SmT1State]
         ## SmT1 is a statemachine with a counter
         count: int
+        defaultCount: int
         s0Count: int
         s1Count: int
 
     ## Forward declare states
+    proc default(sm: ptr SmT1, msg: MsgPtr)
     proc s1(sm: ptr SmT1, msg: MsgPtr)
     proc s0(sm: ptr SmT1, msg: MsgPtr)
+
+    proc default(sm: ptr SmT1, msg: MsgPtr) =
+      ## default state no transition increments counters
+      sm.count += 1
+      sm.defaultCount += 1
+      echo "default: count=", sm.count
+      msg.rspq.add(msg)
 
     proc s0(sm: ptr SmT1, msg: MsgPtr) =
       ## S0 state transitions to S1 increments counter
@@ -72,7 +121,7 @@ when isMainModule:
     proc newSmT1(): ptr SmT1 =
       ## Create a new SmT1 state machine
       result = allocObject[SmT1]()
-      initStateMachine[SmT1, SmT1State](result, "smt1", s0)
+      initStateMachine[SmT1, SmT1State](result, "smt1")
 
     proc delSmT1(sm: ptr SmT1) =
       deinitStateMachine[SmT1State](sm)
@@ -80,13 +129,51 @@ when isMainModule:
     var
       smT1: ptr SmT1
 
+    # Tests default as the one and only state
     setup:
       smT1 = newSmT1()
+      addState[SmT1State](smT1, default)
+      startStateMachine[SmT1State](smT1, default)
 
     teardown:
       smT1.delSmt1()
 
-    test "transition":
+    test "test-one-state":
+      var
+        rcvq = newMpscFifo("rcvq", smT1.ma)
+        msg: MsgPtr
+
+      # Send first message, should be processed by default
+      msg = smT1.ma.getMsg(rcvq, 1)
+      smT1.rcvq.add(msg)
+      msg = rcvq.rmv()
+      check msg.cmd == 1
+      check smt1.count == 1
+      check smt1.defaultCount == 1
+      check smt1.s0Count == 0
+      check smt1.s1Count == 0
+
+      # Send second message, should be processed by default
+      msg = smT1.ma.getMsg(rcvq, 2)
+      smT1.rcvq.add(msg)
+      msg = rcvq.rmv()
+      check msg.cmd == 2
+      check smt1.count == 2
+      check smt1.defaultCount == 2
+      check smt1.s0Count == 0
+      check smt1.s1Count == 0
+
+    # Test with two states s0, s1
+    setup:
+      smT1 = newSmT1()
+      addState[SmT1State](smT1, s0)
+      addState[SmT1State](smT1, s1)
+      startStateMachine[SmT1State](smT1, s0)
+
+    teardown:
+      smT1.delSmt1()
+
+    test "test-two-states":
       var
         rcvq = newMpscFifo("rcvq", smT1.ma)
         msg: MsgPtr
@@ -97,6 +184,7 @@ when isMainModule:
       msg = rcvq.rmv()
       check msg.cmd == 1
       check smt1.count == 1
+      check smt1.defaultCount == 0
       check smt1.s0Count == 1
       check smt1.s1Count == 0
 
@@ -106,5 +194,46 @@ when isMainModule:
       msg = rcvq.rmv()
       check msg.cmd == 2
       check smt1.count == 2
+      check smt1.defaultCount == 0
+      check smt1.s0Count == 1
+      check smt1.s1Count == 1
+
+    # Test with default and two child states s0, s1
+    # TODO: Add passing of unhandled message and verify
+    # TODO: that default is invoked
+    setup:
+      smT1 = newSmT1()
+      addState[SmT1State](smT1, default)
+      addState[SmT1State](smT1, s0, default)
+      addState[SmT1State](smT1, s1, default)
+      addState[SmT1State](smT1, s1, default)
+
+      startStateMachine[SmT1State](smT1, s0)
+
+    teardown:
+      smT1.delSmt1()
+
+    test "test-default-and-two-child-states":
+      var
+        rcvq = newMpscFifo("rcvq", smT1.ma)
+        msg: MsgPtr
+
+      # Send first message, should be processed by S0
+      msg = smT1.ma.getMsg(rcvq, 1)
+      smT1.rcvq.add(msg)
+      msg = rcvq.rmv()
+      check msg.cmd == 1
+      check smt1.count == 1
+      check smt1.defaultCount == 0
+      check smt1.s0Count == 1
+      check smt1.s1Count == 0
+
+      # Send second message, should be processed by S1
+      msg = smT1.ma.getMsg(rcvq, 2)
+      smT1.rcvq.add(msg)
+      msg = rcvq.rmv()
+      check msg.cmd == 2
+      check smt1.count == 2
+      check smt1.defaultCount == 0
       check smt1.s0Count == 1
       check smt1.s1Count == 1
